3c84d887e7b655356bb73d135ef754c7d81650cc
Antonio <antonio@nativapps.mobi>
2017-08-04 18:39:32 -0500

    Initial commit

---
+++ client/reduxstagram.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { render } from 'react-dom';
+
+// import css
+import css from './styles/style.styl';
+import {Main} from './components/Main';
+
+// import components
+render(
+    <Main/>, document.getElementById('root')
+);
+
